f7481cca4bfacef47735194c099d0326c23aafbf
esmyers <emyers@usgs.gov>
2018-08-23 16:05:01 -0500

    reformatting and cleanup

--- src/scripts/layers.js
+++ src/scripts/layers.js
@@ -8,186 +8,186 @@
 var allLayers;
 
 require([
-  "esri/geometry/Extent",
-  "esri/layers/WMSLayerInfo",
-  "esri/layers/FeatureLayer",
-  "esri/layers/ImageParameters",
-  "esri/layers/LayerDrawingOptions",
-  "dojo/domReady!"
+    "esri/geometry/Extent",
+    "esri/layers/WMSLayerInfo",
+    "esri/layers/FeatureLayer",
+    "esri/layers/ImageParameters",
+    "esri/layers/LayerDrawingOptions",
+    "dojo/domReady!"
 ], function(
-  Extent,
-  WMSLayerInfo,
-  FeatureLayer,
-  ImageParameters,
-  LayerDrawingOptions
+    Extent,
+    WMSLayerInfo,
+    FeatureLayer,
+    ImageParameters,
+    LayerDrawingOptions
 ) {
-  var sparrowOverlay;
-  if ($("#radio1")[0].checked == true) {
-    sparrowOverlay = 0;
-  } else {
-    sparrowOverlay = 1;
-  }
-
-  var layerOptions = new LayerDrawingOptions();
-  layerOptions.scaleSymbols = true;
-
-  allLayers = [
-    {
-      groupHeading: "Nutrient Model",
-      showGroupHeading: true,
-      includeInLayerList: true,
-      layers: {
-        "Nutrient Model Results Overlay": {
-          url: serviceBaseURL,
-          visibleLayers: [sparrowOverlay],
-          options: {
-            id: "SparrowRanking",
-            opacity: 0.75,
-            visible: true,
-            layerDrawingOptions: layerOptions
-          },
-          wimOptions: {
-            type: "layer",
-            layerType: "agisDynamic",
-            includeInLayerList: true,
-            hasOpacitySlider: true,
-            hasZoomto: false,
-            includeLegend: false
-          }
-        }
-      }
-    },
-    {
-      groupHeading: "Model Calibration Sites",
-      showGroupHeading: true,
-      includeInLayerList: true,
-      layers: {
-        "Phosphorus Calibration Sites": {
-          url: serviceBaseURL,
-          visibleLayers: [18],
-          options: {
-            id: "phosCalibration",
-            opacity: 0.85,
-            visible: false
-          },
-          wimOptions: {
-            type: "layer",
-            layerType: "agisDynamic",
-            includeInLayerList: true,
-            zoomScale: 144448,
-            hasOpacitySlider: true,
-            hasZoomto: false,
-            includeLegend: true
-          }
-        },
-        "Nitrogen Calibration Sites": {
-          url: serviceBaseURL,
-          visibleLayers: [19],
-          options: {
-            id: "nitroCalibration",
-            visible: false,
-            opacity: 0.85
-          },
-          wimOptions: {
-            type: "layer",
-            layerType: "agisDynamic",
-            includeInLayerList: true,
-            hasOpacitySlider: true,
-            hasZoomto: false,
-            includeLegend: false
-          }
-        }
-      }
-    },
-    {
-      groupHeading: "Auxiliary Layers",
-      showGroupHeading: true,
-      includeInLayerList: true,
-      layers: {
-        "Eastern U.S. Streams": {
-          url: serviceBaseURL,
-          visibleLayers: [21],
-          options: {
-            id: "allStreams",
-            visible: false
-          },
-          wimOptions: {
-            type: "layer",
-            layerType: "agisDynamic",
-            includeInLayerList: true,
-            hasOpacitySlider: true,
-            hasZoomto: false,
-            includeLegend: false
-          }
-        },
-        "Eastern U.S. Streams > 100cfs": {
-          url: serviceBaseURL,
-          visibleLayers: [22],
-          options: {
-            id: "intStreams",
-            visible: false,
-            opacity: 0.35
-          },
-          wimOptions: {
-            type: "layer",
-            layerType: "agisDynamic",
-            includeInLayerList: true,
-            hasOpacitySlider: true,
-            hasZoomto: false,
-            includeLegend: false
-          }
-        },
-        "Eastern U.S. Major Rivers": {
-          url: serviceBaseURL,
-          visibleLayers: [20],
-          options: {
-            id: "streams",
-            visible: false
-          },
-          wimOptions: {
-            type: "layer",
-            layerType: "agisDynamic",
-            includeInLayerList: true,
-            hasOpacitySlider: true,
-            hasZoomto: false,
-            includeLegend: false
-          }
-        },
-        "SPARROW Model Area": {
-          url: serviceBaseURL,
-          visibleLayers: [23],
-          options: {
-            id: "modelArea",
-            visible: false,
-            opacity: 0.35
-          },
-          wimOptions: {
-            type: "layer",
-            layerType: "agisDynamic",
-            includeInLayerList: true,
-            hasOpacitySlider: true,
-            hasZoomto: false,
-            includeLegend: false
-          }
-        },
-        "Land Use 2002": {
-          url:
-            "https://gis.wim.usgs.gov/arcgis/rest/services/SWTrends/lu2002_100515_test/ImageServer",
-          options: {
-            id: "lu2002",
-            opacity: 0.5,
-            visible: false
-          },
-          wimOptions: {
-            type: "layer",
-            layerType: "agisImage",
-            includeInLayerList: true,
-            hasOpacitySlider: true,
-            hasZoomto: false,
-            includeLegend: false
-          }
-        }
-      }
+    var sparrowOverlay;
+    if ($("#radio1")[0].checked == true) {
+        sparrowOverlay = 0;
+    } else {
+        sparrowOverlay = 1;
     }
-  ];
+
+    var layerOptions = new LayerDrawingOptions();
+    layerOptions.scaleSymbols = true;
+
+    allLayers = [
+        {
+            groupHeading: "Nutrient Model",
+            showGroupHeading: true,
+            includeInLayerList: true,
+            layers: {
+                "Nutrient Model Results Overlay": {
+                    url: serviceBaseURL,
+                    visibleLayers: [sparrowOverlay],
+                    options: {
+                        id: "SparrowRanking",
+                        opacity: 0.75,
+                        visible: true,
+                        layerDrawingOptions: layerOptions
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                }
+            }
+        },
+        {
+            groupHeading: "Model Calibration Sites",
+            showGroupHeading: true,
+            includeInLayerList: true,
+            layers: {
+                "Phosphorus Calibration Sites": {
+                    url: serviceBaseURL,
+                    visibleLayers: [18],
+                    options: {
+                        id: "phosCalibration",
+                        opacity: 0.85,
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        zoomScale: 144448,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: true
+                    }
+                },
+                "Nitrogen Calibration Sites": {
+                    url: serviceBaseURL,
+                    visibleLayers: [19],
+                    options: {
+                        id: "nitroCalibration",
+                        visible: false,
+                        opacity: 0.85
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                }
+            }
+        },
+        {
+            groupHeading: "Auxiliary Layers",
+            showGroupHeading: true,
+            includeInLayerList: true,
+            layers: {
+                "Eastern U.S. Streams": {
+                    url: serviceBaseURL,
+                    visibleLayers: [21],
+                    options: {
+                        id: "allStreams",
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "Eastern U.S. Streams > 100cfs": {
+                    url: serviceBaseURL,
+                    visibleLayers: [22],
+                    options: {
+                        id: "intStreams",
+                        visible: false,
+                        opacity: 0.35
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "Eastern U.S. Major Rivers": {
+                    url: serviceBaseURL,
+                    visibleLayers: [20],
+                    options: {
+                        id: "streams",
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "SPARROW Model Area": {
+                    url: serviceBaseURL,
+                    visibleLayers: [23],
+                    options: {
+                        id: "modelArea",
+                        visible: false,
+                        opacity: 0.35
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "Land Use 2002": {
+                    url:
+                        "https://gis.wim.usgs.gov/arcgis/rest/services/SWTrends/lu2002_100515_test/ImageServer",
+                    options: {
+                        id: "lu2002",
+                        opacity: 0.5,
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisImage",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                }
+            }
+        }
+    ];
 });
